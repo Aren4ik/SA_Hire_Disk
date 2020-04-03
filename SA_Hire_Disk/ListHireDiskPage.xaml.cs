a327e714f03d34aec60d9ebadfef2165c2d69ff2
Aren4ik <aren20228@gmail.com>
2020-04-03 19:54:59 +0300

    Создал страницу  "Список арендованых дисков"

---
+++ SA_Hire_Disk/ListHireDiskPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SA_Hire_Disk
+{
+    /// <summary>
+    /// Логика взаимодействия для ListHireDiskPage.xaml
+    /// </summary>
+    public partial class ListHireDiskPage : Page
+    {
+        public ListHireDiskPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
